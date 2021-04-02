2f39f3040fa3c3e22d9da7353792773773112471
AlexandrBear <nepr.sasha@gmail.com>
2021-04-02 21:03:36 +0300

    Изменение информации о себе

--- app/src/main/java/com/example/mytelegram/ui/fragments/ChangeUsernameFragment.kt
+++ app/src/main/java/com/example/mytelegram/ui/fragments/ChangeUsernameFragment.kt
@@ -1,37 +1,21 @@
 package com.example.mytelegram.ui.fragments
 
-import android.os.Bundle
-import android.view.*
-import androidx.fragment.app.Fragment
-import com.example.mytelegram.MainActivity
 import com.example.mytelegram.R
 import com.example.mytelegram.utilits.*
 import kotlinx.android.synthetic.main.fragment_change_username.*
 import java.util.*
 
 
-class ChangeUsernameFragment : BaseFragment(R.layout.fragment_change_username) {
+class ChangeUsernameFragment : BaseChangeFragment(R.layout.fragment_change_username) {
 
    lateinit var mNewUsername:String
 
     override fun onResume() {
         super.onResume()
-        setHasOptionsMenu(true)
         settings_input_username.setText(USER.username)
-
-    }
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        (activity as MainActivity).menuInflater.inflate(R.menu.settings_mamu_confirm,menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.settings_confirm_change -> change()
-        }
-        return true
-    }
-
-    private fun change() {
+    override fun change() {
         mNewUsername = settings_input_username.text.toString().toLowerCase(Locale.getDefault())
         if (mNewUsername.isEmpty()){
             showToast("Поле пустое")
